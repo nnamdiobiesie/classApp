65c3ff7b508bd25d84e358311fe00b6726ff318d
nnamdiobiesie <nnamdi_obiesie@yahoo.com>
2017-11-15 15:41:50 +0100

    How to validate for values !in_array

--- www/test.php
+++ www/test.php
@@ -1,23 +1,34 @@
 <?php
 
-				define('MAX_FILE_SIZE', '2097152')
+				define('MAX_FILE_SIZE', '2097152');
+				$ext = ['image/jpg', 'image/jpeg', 'image/png'];
 			if(array_key_exists('save', $_POST)) {
 
 						//print_r($_FILES);
 
 				$errors = [];
 
-				if(empty($_POST['pics']['name'])) {
+				if(empty($_FILES['pics']['name'])) {
 
 							$error[] = "Please select a file";
 
 				}
 
-						if($_POST['pics']['size'] > MAX_FILE_SIZE){
+						if($_FILES['pics']['size'] > MAX_FILE_SIZE){
 
 
 							$error[] = "File too large. Maximum:  ".MAX_FILE_SIZE;
 						}
+
+						//IN_ARRAY CHECKS TO SEE IF THE VALUES SAVED IN THE EXT ARRAY, if it EXISTS IN THAT ARRAY.
+						if(!in_array($_FILES['pics']['type'],$ext)){
+							$error[] = "File format not supported";
+
+
+
+						}
+
+						$rnd = rand(0000000000, 9999999999);
 
 					if(empty($error)){
 
