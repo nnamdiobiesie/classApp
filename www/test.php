31c8c9a2ba7264520fc1101bae796007c8a5f525
nnamdiobiesie <nnamdi_obiesie@yahoo.com>
2017-11-15 16:15:08 +0100

    New Update

--- www/test.php
+++ www/test.php
@@ -18,17 +18,29 @@
 
 
 							$error[] = "File too large. Maximum:  ".MAX_FILE_SIZE;
+							$_FILES['pics']['tmp_name'] = null;
 						}
 
 						//IN_ARRAY CHECKS TO SEE IF THE VALUES SAVED IN THE EXT ARRAY, if it EXISTS IN THAT ARRAY.
 						if(!in_array($_FILES['pics']['type'],$ext)){
 							$error[] = "File format not supported";
-
+							$_FILES['pics']['tmp_name'] = null;
 
 
 						}
 
-						$rnd = rand(0000000000, 9999999999);
+						$rnd = rand(0000000000, 9999999999); //THIS GENERATES RANDOM 10 NUMBERS
+						$strip_name = str_replace(' ','_',$_FILES['pics']['name']);
+
+						$filename = $rnd.$strip_name; //ThIS TIES EACH FILE UPLOADED BY THE USER TO A RANDOM NUMBER
+						$destination = './uploads/'.$filename;
+
+
+						IF(!move_uploaded_file($_FILES['pics']['tmp_name'],$destination)){
+
+							$errors[] = "File not uploaded";
+
+						}
 
 					if(empty($error)){
 
