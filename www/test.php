e8769757e3d9ddbd293bd8eaccaf0497af0331d8
nnamdiobiesie <nnamdi_obiesie@yahoo.com>
2017-11-15 15:18:07 +0100

    Newer ways to validate php codes

--- www/test.php
+++ www/test.php
@@ -1,3 +1,47 @@
+<?php
+
+				define('MAX_FILE_SIZE', '2097152')
+			if(array_key_exists('save', $_POST)) {
+
+						//print_r($_FILES);
+
+				$errors = [];
+
+				if(empty($_POST['pics']['name'])) {
+
+							$error[] = "Please select a file";
+
+				}
+
+						if($_POST['pics']['size'] > MAX_FILE_SIZE){
+
+
+							$error[] = "File too large. Maximum:  ".MAX_FILE_SIZE;
+						}
+
+					if(empty($error)){
+
+						echo  "File upload successful";
+
+					}else {
+
+
+						foreach ($error as $err){
+
+								echo $err.'</br>';
+
+						}
+					}
+
+
+			}
+
+
+?>
+
+
+
+
 <form id="register" method="POST", enctype="multipart/form-data">
 		<p> Please upload a picture </p>
 		<input type="file" name="pics">
