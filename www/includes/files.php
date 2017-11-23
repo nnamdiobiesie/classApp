30f645df1864d1fe417919a5a207a84782550c34
nnamdiobiesie <nnamdi_obiesie@yahoo.com>
2017-11-23 13:45:38 +0100

    Functions script

--- www/includes/files.php
+++ www/includes/files.php
@@ -15,6 +15,7 @@ function uploadFile ($files, $name, $loc){
 			{
 
 						$result[] = true;
+						$result[] = $destination;
 			}else {
 				$result[] = false;
 
@@ -83,14 +84,370 @@ function doesEmailExist($dbconn, $email) {
 
 
 
+}
 
 
 
 
+function displayErrors($err, $name)
+{
 
+
+		$result = "";
+
+
+		if(isset($err[$name])){
+
+			$result = '<span class=err>'.$err[$name].'</span>';
+		}
+
+				return $result;
 
 
 }
+
+
+
+
+function adminLogin($dbconn, $input) {
+
+		$result = [];
+
+
+		$stmt = $dbconn->prepare("SELECT * FROM admin WHERE email=:e");
+
+
+
+		
+		$stmt ->bindParam(':e', $input['email']);
+		$stmt ->execute();
+
+
+		$count =  $stmt->rowCount();
+		$row = $stmt->fetch(PDO::FETCH_BOTH);
+
+
+				If($count != 1 || !password_verify($input['password'], $row['hash'])){
+
+
+							$result[] = false;
+						}else {
+
+
+								$result[] = true;
+								$result [] = $row;
+
+						}
+
+
+								return $result;
+
+					}
+				
+		
+
+
+
+
+
+
+
+		function addCategory($dbconn, $input) {
+
+
+					$stmt = $dbconn->prepare("INSERT INTO category(category_name) VALUES(:catName)");
+
+					$stmt->bindParam(':catName', $input['cat_name']);
+
+					$stmt->execute();
+
+
+		}
+
+		    function checkLogin() {
+
+
+		    		if(isset($_SESSION['aid'])){
+
+
+		    				header("Location: login.php");
+
+
+		    		}
+
+
+
+
+
+
+		    }
+
+
+
+		    function redirect ($location, $msg){
+
+
+		    		header("location: ".$location.$msg);
+
+		    }
+
+
+		    function viewCategory($dbconn){
+
+		    		$result = "";
+
+
+		    		$stmt = $dbconn->prepare("SELECT * FROM category");
+
+
+		    		$stmt->execute();
+
+
+		    		while($row = $stmt->fetch(PDO::FETCH_BOTH)){
+
+		    			$result .= '<tr><td>'.$row[0].'</td>';
+
+		    			$result .= '<td>'.$row[1].'</td>';
+
+		    			$result .= '<td><a href="edit_category.php?cat_id='.$row[0].'">edit</a></td>';
+
+		    			$result .= '<td><a href="delete_category.php?cat_id='.$row[0].'">delete</a></td></tr>';
+
+		    		}
+
+		    			return $result;
+
+		    }
+
+
+		    	function getCategoryById($dbconn, $id){
+		    		$stmt = $dbconn->prepare("SELECT * FROM category WHERE category_id=:catId");
+
+		    		$stmt->bindParam(':catId', $id);
+
+		    		$stmt->execute();
+
+
+		    		$row = $stmt->fetch(PDO::FETCH_BOTH);
+
+
+		    		return $row;
+
+
+
+		    	}
+
+
+
+
+
+		    	function  updateCategory($dbconn, $input){
+
+		    		$stmt = $dbconn->prepare("UPDATE category SET category_name=:catName WHERE category_id=:catId");
+
+
+		    		$data = [
+
+		    				":catName" => $input['cat_name'],
+		    				":catId" => $input['id']
+
+		    		];
+
+		    		$stmt->execute($data);
+
+
+
+		    	}
+
+
+				//TO HOVER ON THE SELECTED HEADER LINK 
+		    	function curNave($page) {
+
+		    			$curPage = basename($_SERVER['SCRIPT_FILENAME']);
+
+
+		    			if($curPage == $page) {
+
+		    						echo 'class="selected"';
+
+
+		    			}
+
+		    	}
+
+
+
+
+
+		    function addProduct($dbconn, $input) {
+
+
+					$stmt = $dbconn->prepare("INSERT INTO books(title, author, price, publication_date, category_id, flag, img_path)
+												 VALUES(:t, :a, :p, :y, :c, :f, :img)");
+
+					//$stmt->bindParam(':catName', $input['cat_name']);
+					$data = [
+							":t" =>$input['title'],
+		    				":a" =>$input['author'],
+		    				":p" =>$input['price'],
+		    				":y" =>$input['year'],
+		    				":c" =>$input['cat'],
+		    				":f" =>$input['flag'],
+		    				":img" =>$input['dest']
+					];
+
+					$stmt->execute($data);
+
+
+		}
+
+
+		function  deleteCategory($dbconn, $id) {
+
+		    		$stmt = $dbconn->prepare("DELETE FROM category WHERE category_id=:catId");
+
+
+
+
+		    			$stmt->bindParam(':catId', $id);
+		    	
+
+		    		$stmt->execute();
+
+
+
+		    	}
+
+
+
+		   function fetchCategory($dbconn, $val=null){
+
+		   		$result = "";
+
+		   		$stmt = $dbconn->prepare("SELECT * FROM category");
+
+		   		$stmt->execute();
+
+
+		   		while($row = $stmt->fetch(PDO::FETCH_BOTH)){
+
+		   			if($val == $row[1]){
+
+		   				continue; //skips the particular value and goes on to the next
+		   			}
+
+
+
+
+		   				$result .= '<option value="'.$row[0].'">'.$row[1].'</option>';
+		   		}
+		   			return $result;
+
+
+		   }
+
+
+		   function viewProducts ($dbconn){
+
+		   		$result = "";
+
+		   		$stmt = $dbconn->prepare("SELECT * from books");
+
+		   		$stmt->execute();
+
+
+		   		while ($row = $stmt->fetch(PDO::FETCH_BOTH)){
+
+
+		   			$result .= '<tr><td>'.$row[1].'</td>';
+		   			$result .= '<td>'.$row[2].'</td>';
+		   			$result .= '<td>'.$row[3].'</td>';
+		   			$result .= '<td>'.$row[5].'</td>';
+		   			$result .= '<td><img src="'.$row[7].'" height="50" width="50"></td>';
+		   			$result .= '<td><a href="edit_products.php?book_id='.$row[0].'">edit</a></td>';
+		   			$result .= '<td><a href="delete_products.php?book_id='.$row[0].'">delete</a></td></tr>';
+		   		}
+
+
+
+		   				return $result;
+
+		   }
+
+
+
+
+
+		   function getProductById($dbconn, $id){
+		    		$stmt = $dbconn->prepare("SELECT * FROM books WHERE book_id=:bookId");
+
+		    		$stmt->bindParam(':bookId', $id);
+
+		    		$stmt->execute();
+
+
+		    		$row = $stmt->fetch(PDO::FETCH_BOTH);
+
+
+		    		return $row;
+
+
+
+		    	}
+
+
+
+		   /*function  updateProduct($dbconn, $input){
+
+		    		$stmt = $dbconn->prepare("UPDATE books SET title=:t, author=:a, price=:p, publication_date=:y,
+
+		    		 							category_id=:c, flag=:f, img_path=:img WHERE book_id=:bid");
+
+
+		    		$data = [
+
+		    				
+		    				":t" =>$input['title'],
+		    				":a" =>$input['author'],
+		    				":p" =>$input['price'],
+		    				":y" =>$input['year'],
+		    				":c" =>$input['cat'],
+		    				":f" =>$input['flag'],
+		    				":img" =>$input['dest'],
+		    				":bid" => $input['id']
+		    			
+
+		    		];
+
+		    		$stmt->execute($data);
+
+
+
+		    	} */
+
+
+
+		    	function editProduct($dbconn, $input){
+
+		    			$stmt = $dbconn->prepare("UPDATE books SET title=:t, author = :a, price=:p, publication_date=:pub, category_id =:catId WHERE  BOOK_ID = :bid");
+
+
+		    			$data = [
+
+		    					":t" => $input['title'],
+		    					":a" => $input['author'],
+		    					":p" => $input['price'],
+		    					":pub" => $input['year'],
+		    					":catId" => $input['cat'],
+		    					":bid" => $input['id']
+
+
+
+		    			];
+
+						$stmt->execute($data);
+		    	}
+
 
 
 
